12066a58cf072b0efbaa69fa26d2399dd4b43a53
dhirajshahninja <dhiraj.shah.ninja@gmail.com>
2019-07-27 15:26:02 +0530

    Added react-pivot data table

--- report.jsx
+++ report.jsx
@@ -1,11 +1,79 @@
-var React = require('react')
-var ReactPivot = require('react-pivot')
-var createReactClass = require('create-react-class')
+'use strict';
 
-var rows = require('./data.json')
+const React = require('react');
+const ReactPivot = require('react-pivot');
+const createReactClass = require('create-react-class');
+
+const rows = require('./data.json');
+
+let dimensions = [
+  {value: 'date', title: 'Date'},
+  {value: 'host', title: 'Host'}
+];
+
+let reduce = function (row, memo) {
+  if (row.type === 'impression') memo.impressions = ++memo.impressions || 1;
+  if (row.type === 'display') memo.displays = ++memo.displays || 1;
+  if (row.type === 'load') memo.loads = ++memo.loads || 1;
+  return memo;
+};
+
+let calculations = [
+  {
+    title: 'Impression',
+    value: 'impressions',
+    template:  (val) => {
+      return val.toString()
+    }
+  },
+  {
+    title: 'Loads',
+    value: 'loads',
+    template: (val) => {
+      return val.toString()
+    }
+  },
+  {
+    title: 'Displays',
+    value: 'displays',
+    template: (val) => {
+      return val.toString()
+    }
+  },
+  {
+    title: 'Load Rate',
+    value: (row) => {
+      return ((row.loads / row.impressions) * 100)
+    },
+    template: (val) => {
+      return val.toFixed(1) + '%'
+    }
+  },
+  {
+    title: 'Display Rate',
+    value: (row) => {
+      return ((row.displays / row.loads) * 100)
+    },
+    template: (val) => {
+      return val.toFixed(1) + '%'
+    },
+    className: 'react-pivot-display-date'
+  }
+];
 
 module.exports = createReactClass({
   render () {
-    return <div>Report</div>
+    return (
+      <div>
+        <ReactPivot
+          rows={rows}
+          dimensions={dimensions}
+          calculations={calculations}
+          reduce={reduce}
+          activeDimensions={['Date', 'Host']}
+          nPaginateRows={10}
+        />
+      </div>
+    )
   }
-})
+});
